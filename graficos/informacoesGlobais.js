48c95bac5390c3787eea63a8299c1d55360f3ce2
helloisad <105357892+helloisad@users.noreply.github.com>
2024-10-11 08:42:21 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,14 +1,15 @@
 const url ='https;//raw.githubusercontent.com/guilhermeorails/api/main/dados-globais.json'
+
 async function visualizarInformacoesGlobais(){
     const res=await fetch(url)
     const dados=res.json()
     const paragrafo=document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML=`você sabia que o mundo tem ${dados.total_pessoas_mundo}de pessoas que aproximadamente ${dados.total_pessoas_conectadas}
-    ${dados.tempo_medio} horas conectadas.`
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML=`você sabia que o mundo tem ${dados.total_pessoas_mundo}de pessoas que aproximadamente ${dados.total_pessoas_conectadas}${dados.tempo_medio} horas conectadas.`
     console.log(paragrafo)
 
 const container=document.getElementById('graficos-container')
 container.appendChild(paragrafo)
 
 }
+visualizarInformacoesGlobais()
