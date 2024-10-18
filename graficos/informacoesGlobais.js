19faee13331034d8ec076a4fd14ecbcdd1d4cc35
helloisad <105357892+helloisad@users.noreply.github.com>
2024-10-18 08:40:52 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function visualizarInformacoesGlobais(){
     const dados=res.json()
     const paragrafo=document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML=`você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span>de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span>de pessoas nas redes sociais e média <span>${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML=`você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> de pessoas nas redes sociais e média <span>${dados.tempo_medio}</span> horas conectadas.`
     
 
 const container=document.getElementById('graficos-container')
